54b97d81e0a9848eefabede95470f60abaee5aa3
daxus4 <61798113+daxus4@users.noreply.github.com>
2024-10-05 17:36:29 +0200

    finished time prediction model

--- time_prediction_model/moving_average_time_prediction_model.py
+++ time_prediction_model/moving_average_time_prediction_model.py
@@ -1,24 +1,45 @@
-from typing import List
+from typing import Dict, List
 
 import numpy as np
 
+from time_prediction_model.period_for_simulation import PeriodForSimulation
 from time_prediction_model.time_prediction_model import TimePredictionModel
 
 
 class MovingAverageTimePredictionModel(TimePredictionModel):
     def predict_next_event_time_from_current_time(
         self,
-        event_times: List[np.ndarray],
+        period_for_simulation: PeriodForSimulation,
         current_time: float,
+    ) -> Dict[str, float]:
+        event_type_event_times_map_to_predict = (
+            period_for_simulation.get_event_type_event_times_map_to_predict()
+        )
+
+        next_event_times = {
+            event_type: self._get_predicted_event_after_current_time(
+                event_times[-1], current_time, self._parameters["window_duration_seconds"]
+            )
+            for event_type, event_times in event_type_event_times_map_to_predict
+        }
+
+        return next_event_times
+
+    def _get_predicted_event_after_current_time(
+        self,
+        event_times: np.ndarray,
+        current_time: float,
+        window_duration_seconds: float
     ) -> float:
-        event_times = event_times[0]
-        event_times = event_times[
-            event_times >= current_time - self._parameters["window_duration_seconds"]
+        event_times_in_window = event_times[
+            event_times >= current_time - window_duration_seconds
         ]
 
-        mean_next_event_time_jump = self._get_mean_next_event_time_jump(event_times)
+        mean_next_event_time_jump = self._get_mean_next_event_time_jump(
+            event_times_in_window
+        )
 
-        next_event_time = event_times[-1] + mean_next_event_time_jump
+        next_event_time = event_times_in_window[-1] + mean_next_event_time_jump
 
         while next_event_time < current_time:
             next_event_time += mean_next_event_time_jump
--- time_prediction_model/naive_time_prediction_model.py
+++ time_prediction_model/naive_time_prediction_model.py
@@ -1,19 +1,37 @@
-from typing import List
+from typing import Dict, List
 
 import numpy as np
 
+from time_prediction_model.period_for_simulation import PeriodForSimulation
 from time_prediction_model.time_prediction_model import TimePredictionModel
 
 
 class NaiveTimePredictionModel(TimePredictionModel):
     def predict_next_event_time_from_current_time(
         self,
-        event_times: List[np.ndarray],
+        period_for_simulation: PeriodForSimulation,
         current_time: float,
+    ) -> Dict[str, float]:
+        event_type_event_times_map_to_predict = (
+            period_for_simulation.get_event_type_event_times_map_to_predict()
+        )
+
+        next_event_times = {
+            event_type: self._get_predicted_event_after_current_time(
+                event_times[-1], current_time, self._parameters["next_event_time_jump"]
+            )
+            for event_type, event_times in event_type_event_times_map_to_predict
+        }
+
+        return next_event_times
+    
+    def _get_predicted_event_after_current_time(
+        self,
+        last_event_time: float,
+        current_time: float,
+        next_event_time_jump: float
     ) -> float:
-        next_event_time = event_times[0][-1] + self._parameters["next_event_time_jump"]
-
+        next_event_time = last_event_time + next_event_time_jump
         while next_event_time < current_time:
-            next_event_time += self._parameters["next_event_time_jump"]
-
+            next_event_time += next_event_time_jump
         return next_event_time
