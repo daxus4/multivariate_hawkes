573473d854ea66e9b212af0f1be0d26a0b856dbc
Davide Raffaelli <61798113+daxus4@users.noreply.github.com>
2025-01-10 18:57:17 +0100

    likelihood and lshade

---
+++ optimization/hawkes_likelihood.py
@@ -0,0 +1,216 @@
+import numpy as np
+from numba import njit
+from numba.typed import List
+
+
+@njit
+def negative_exponential_effect(
+    beta_mn: float, end_time: float, times_n: np.ndarray
+) -> float:
+    return np.sum(1 - np.exp(-beta_mn * (end_time - times_n)))
+
+
+@njit
+def loglikelihood_negative_exponential_contribution(
+    alphas_mn: np.ndarray,
+    betas_mn: np.ndarray,
+    end_time: float,
+    events_times: List[np.ndarray],
+) -> float:
+    num_events = len(events_times)
+    negative_exponential_effects = np.empty(num_events, dtype=np.float64)
+
+    for n in range(num_events):
+        negative_exponential_effects[n] = negative_exponential_effect(
+            betas_mn[n], end_time, events_times[n]
+        )
+
+    return np.sum(alphas_mn / betas_mn * negative_exponential_effects)
+
+
+@njit
+def r_mn(
+    beta_mn: float,
+    current_t_m: float,
+    previous_t_m: float,
+    previous_r_mn: float,
+    intermediate_t_n_values: np.ndarray,
+) -> float:
+    return np.exp(-beta_mn * (current_t_m - previous_t_m)) * previous_r_mn + np.sum(
+        np.exp(-beta_mn * (current_t_m - intermediate_t_n_values))
+    )
+
+
+@njit
+def counting_process_integral_subfunction(
+    mu_m: float,
+    alphas_mn: np.ndarray,
+    rs_mn: np.ndarray,
+) -> float:
+    return np.log(mu_m + np.sum(alphas_mn * rs_mn))
+
+
+@njit
+def loglikelihood_m(
+    m_index: int,
+    mu_m: float,
+    negative_exponential_contribution: float,
+    alphas_mn: np.ndarray,
+    betas_mn: np.ndarray,
+    end_time: float,
+    events_times: List[np.ndarray],
+) -> float:
+    num_events = len(events_times)
+
+    dt_integral = -mu_m * end_time - negative_exponential_contribution
+
+    recursive_part_effects = np.zeros(num_events, dtype=np.float64)
+    end_indices_times_events = np.zeros(num_events, dtype=np.float64)
+
+    for n in range(num_events):
+        end_indices_times_events[n] = np.searchsorted(
+            events_times[n], events_times[m_index][0], side="left"
+        )
+
+        recursive_part_effects[n] = r_mn(
+            betas_mn[n],
+            events_times[m_index][0],
+            0,
+            recursive_part_effects[n],
+            events_times[n][: end_indices_times_events[n]],
+        )
+
+    counting_process_integral = counting_process_integral_subfunction(
+        mu_m,
+        alphas_mn,
+        recursive_part_effects,
+    )
+
+    for k in range(1, len(events_times[m_index])):
+        start_indices_times_events = end_indices_times_events.copy()
+
+        for n in range(num_events):
+            current_t_m = events_times[m_index][k]
+            previous_t_m = events_times[m_index][k - 1]
+
+            end_indices_times_events[n] = (
+                np.searchsorted(
+                    events_times[n][start_indices_times_events[n] :],
+                    current_t_m,
+                    side="left",
+                )
+                + start_indices_times_events[n]
+            )
+
+            recursive_part_effects[n] = r_mn(
+                betas_mn[n],
+                current_t_m,
+                previous_t_m,
+                recursive_part_effects[n],
+                events_times[n][
+                    start_indices_times_events[n] : end_indices_times_events[n]
+                ],
+            )
+
+        counting_process_integral += counting_process_integral_subfunction(
+            mu_m,
+            alphas_mn,
+            recursive_part_effects,
+        )
+
+    return dt_integral + counting_process_integral
+
+
+@njit
+def loglikelihood(
+    mu: np.ndarray,
+    alphas: np.ndarray,
+    betas: np.ndarray,
+    end_time: float,
+    events_times: List[np.ndarray],
+) -> float:
+    num_events = len(events_times)
+    loglikelihood_value = 0
+
+    negative_exponential_contributions = np.empty(num_events, dtype=np.float64)
+
+    for n in range(num_events):
+        negative_exponential_contributions[n] = (
+            loglikelihood_negative_exponential_contribution(
+                alphas[n], betas[n], end_time, events_times[n]
+            )
+        )
+
+    for m in range(num_events):
+        loglikelihood_value += loglikelihood_m(
+            m,
+            mu[m],
+            negative_exponential_contributions[m],
+            alphas[m],
+            betas[m],
+            end_time,
+            events_times,
+        )
+
+    return loglikelihood_value
+
+
+@njit
+def l1_penalty(
+    alphas: np.ndarray, betas: np.ndarray, regularization_param: float
+) -> float:
+    return regularization_param * (np.sum(np.abs(alphas)) + np.sum(np.abs(betas)))
+
+
+@njit
+def instability_penalty(rhos: np.ndarray, instability_param: float) -> float:
+    kernel_spectral_norm = np.linalg.norm(rhos, ord=2)
+    if kernel_spectral_norm > 1:
+        return instability_param * kernel_spectral_norm
+    else:
+        return 0
+
+
+@njit
+def likelihood_fitness_to_minimize(
+    mu: np.ndarray,
+    alphas: np.ndarray,
+    betas: np.ndarray,
+    rhos: np.ndarray,
+    end_time: float,
+    events_times: List[np.ndarray],
+    regularization_param: float,
+    instability_param: float,
+) -> float:
+    return (
+        -loglikelihood(mu, alphas, betas, end_time, events_times)
+        + l1_penalty(alphas, betas, regularization_param)
+        + instability_penalty(rhos, instability_param)
+    )
+
+
+@njit
+def get_likelihood_fitness_from_individual(
+    individual: np.ndarray,
+    end_time: float,
+    events_times: List[np.ndarray],
+    regularization_param: float,
+    instability_param: float,
+) -> float:
+    num_event_types = len(events_times)
+
+    mu = individual[:num_event_types]
+    alphas = individual[num_event_types : 2 * num_event_types]
+    betas = individual[2 * num_event_types : 3 * num_event_types]
+    rhos = alphas / betas
+
+    return likelihood_fitness_to_minimize(
+        mu,
+        alphas,
+        betas,
+        rhos,
+        end_time,
+        events_times,
+        regularization_param,
+        instability_param,
+    )
--- optimization/lshade.py
+++ optimization/lshade.py
@@ -23,57 +23,71 @@ def get_initial_random_population(
 
     # fill the initial population with random mu values
     for i in range(type_count):
-        current_mu_max_value = 0.2*event_counts_for_type[i]/training_time_in_seconds
+        current_mu_max_value = 0.2 * event_counts_for_type[i] / training_time_in_seconds
         initial_population[:, i] = np.random.uniform(
-            0, min(gene_upper_boundaries[i], current_mu_max_value), initial_population_size
+            0,
+            min(gene_upper_boundaries[i], current_mu_max_value),
+            initial_population_size,
         )
 
     # fill the initial population with random rho values
-    for i,j in [(i,j) for i in range(type_count) for j in range(type_count)]:
+    for i, j in [(i, j) for i in range(type_count) for j in range(type_count)]:
         current_rho_max_value = min(
-            0.2*event_counts_for_type[i]/event_counts_for_type[j],
-            0.5
+            0.2 * event_counts_for_type[i] / event_counts_for_type[j], 0.5
         )
 
-        initial_population[:, type_count + i*type_count + j] = np.random.uniform(
+        initial_population[:, type_count + i * type_count + j] = np.random.uniform(
             0,
-            min(gene_upper_boundaries[type_count + i*type_count + j], current_rho_max_value),
-            initial_population_size
+            min(
+                gene_upper_boundaries[type_count + i * type_count + j],
+                current_rho_max_value,
+            ),
+            initial_population_size,
         )
-    
-    # rescale rho for individual i such that l2 norm of rho_i is less than 1
-    #for i in range(type_count):
-    #    rho_indices = [type_count + i*type_count + j for j in range(type_count)]
-    #    rho_values = initial_population[:, rho_indices]
-    #    rho_norms = np.linalg.norm(rho_values, axis=1)
-    #    rho_values = rho_values / rho_norms[:, np.newaxis]
 
-    #initial_population[:, rho_indices] = rho_values
-    
+    # rescale the rho values for individuals with spectral norm > 1
+    for individual_index in range(initial_population_size):
+        rho_values = initial_population[
+            individual_index, type_count : type_count + square_matrix_size
+        ].reshape((type_count, type_count))
+        kernel_spectral_norm = np.linalg.norm(rho_values, ord=2)
+        if kernel_spectral_norm > 1:
+            random_factor = np.random.uniform(0, 1)
+
+            rescaled_rho_matrix = rho_values * (random_factor / kernel_spectral_norm)
+
+            initial_population[
+                individual_index, type_count : type_count + square_matrix_size
+            ] = rescaled_rho_matrix.flatten()
+
     # fill the initial population with random beta values
-    for i,j in [(i,j) for i in range(type_count) for j in range(type_count)]:
+    for i, j in [(i, j) for i in range(type_count) for j in range(type_count)]:
         bernoulli_samples = np.random.binomial(n=1, p=0.5, size=initial_population_size)
 
         random_values = np.where(
             bernoulli_samples == 0,
             np.random.uniform(0, 1, size=initial_population_size),
-            np.random.uniform(0, 100, size=initial_population_size)
+            np.random.uniform(0, 100, size=initial_population_size),
         )
 
-        initial_population[:, type_count + square_matrix_size + i*type_count + j] = random_values
+        initial_population[:, type_count + square_matrix_size + i * type_count + j] = (
+            random_values
+        )
 
     return initial_population
+
 
 def weighted_lehmar_mean(succeding_rates, individual_fitnesses, trial_fitnesses):
     improvements = individual_fitnesses - trial_fitnesses
     total_improvement = np.sum(improvements)
     weights = improvements / total_improvement
 
-    lehmar_mean = np.sum(
-        weights * succeding_rates**2
-    ) / np.sum(weights * succeding_rates)
+    lehmar_mean = np.sum(weights * succeding_rates**2) / np.sum(
+        weights * succeding_rates
+    )
 
     return lehmar_mean
+
 
 def lshade(
     fitness: Callable[[np.ndarray], float],
@@ -81,14 +95,13 @@ def lshade(
     gene_lower_boundaries: np.ndarray,
     gene_upper_boundaries: np.ndarray,
     initial_population: np.ndarray,
-    initial_population_size: int = 100,
     max_generations: int = 100,
     memory_size: int = 10,
     p: float = 0.2,
     max_number_fitness_evaluations: int = 1000,
 ) -> Tuple[np.ndarray, float]:
-    population_size = initial_population_size
-    archive_size = initial_population_size
+    population_size = initial_population.shape[0]
+    archive_size = initial_population.shape[0]
     archive = np.zeros((archive_size, problem_dimensionality))
     archive_fitnesses = np.empty(archive_size)
     archive_fitnesses.fill(np.nan)
@@ -102,7 +115,9 @@ def lshade(
     crossing_rates = np.zeros(population_size)
     mutation_rates = np.zeros(population_size)
 
-    previous_gen_individuals_fitnesses = np.array([fitness(individual) for individual in previous_gen_population])
+    previous_gen_individuals_fitnesses = np.array(
+        [fitness(individual) for individual in previous_gen_population]
+    )
     current_number_of_fitness_evaluations = population_size
     memory_index_to_update = 0
 
@@ -111,12 +126,16 @@ def lshade(
 
     best_individual_fitness_index = np.argmin(previous_gen_individuals_fitnesses)
     global_best_individual = previous_gen_population[best_individual_fitness_index]
-    global_best_individual_fitness = previous_gen_individuals_fitnesses[best_individual_fitness_index]
+    global_best_individual_fitness = previous_gen_individuals_fitnesses[
+        best_individual_fitness_index
+    ]
 
-    while (generation_number <= max_generations) and (current_number_of_fitness_evaluations < max_number_fitness_evaluations):
-        
+    while (generation_number <= max_generations) and (
+        current_number_of_fitness_evaluations < max_number_fitness_evaluations
+    ):
+
         succeding_crossover_rates = np.empty(population_size)
-        succeding_crossover_rates.fill(np.nan)  
+        succeding_crossover_rates.fill(np.nan)
         succeding_mutation_rates = np.empty(population_size)
         succeding_mutation_rates.fill(np.nan)
         succeding_trials = np.empty((population_size, problem_dimensionality))
@@ -136,15 +155,13 @@ def lshade(
                 crossing_rates[individual_index] = np.clip(
                     np.random.normal(memory[memory_index, MEMORY_MCR_INDEX], 0.1),
                     0,
-                    1.0
+                    1.0,
                 )
 
             generated_mutation_rate = 0
             while generated_mutation_rate <= 0:
                 generated_mutation_rate = np.clip(
-                    cauchy.rvs(memory[memory_index, MEMORY_MMR_INDEX],0.1),
-                    None,
-                    1.0
+                    cauchy.rvs(memory[memory_index, MEMORY_MMR_INDEX], 0.1), None, 1.0
                 )
             mutation_rates[individual_index] = generated_mutation_rate
 
@@ -163,10 +180,11 @@ def lshade(
 
             number_individuals_in_archive = np.sum(~np.isnan(archive_fitnesses))
             individual_indices_not_current = [
-                index for index in range(population_size + number_individuals_in_archive)
+                index
+                for index in range(population_size + number_individuals_in_archive)
                 if index != individual_index
             ]
-            random_individual_indices =np.random.choice(
+            random_individual_indices = np.random.choice(
                 individual_indices_not_current, 2, replace=False
             )
 
@@ -182,107 +200,151 @@ def lshade(
             )
             current_individual = previous_gen_population[individual_index]
 
-            mutant = current_individual + mutation_rates[individual_index] * (
-                best_individual - current_individual
-            ) + mutation_rates[individual_index] * (
-                random_individual_1 - random_individual_2
+            mutant = (
+                current_individual
+                + mutation_rates[individual_index]
+                * (best_individual - current_individual)
+                + mutation_rates[individual_index]
+                * (random_individual_1 - random_individual_2)
             )
 
             for gene_index in range(problem_dimensionality):
                 if mutant[gene_index] < gene_lower_boundaries[gene_index]:
                     mutant[gene_index] = (
-                        gene_lower_boundaries[gene_index] + current_individual[gene_index]
+                        gene_lower_boundaries[gene_index]
+                        + current_individual[gene_index]
                     ) / 2
                 elif mutant[gene_index] > gene_upper_boundaries[gene_index]:
                     mutant[gene_index] = (
-                        gene_upper_boundaries[gene_index] + current_individual[gene_index]
+                        gene_upper_boundaries[gene_index]
+                        + current_individual[gene_index]
                     ) / 2
 
             random_gene_index_to_mutate = np.random.randint(0, problem_dimensionality)
-            crossover_gene_index = np.random.rand(problem_dimensionality) < crossing_rates[individual_index]
+            crossover_gene_index = (
+                np.random.rand(problem_dimensionality)
+                < crossing_rates[individual_index]
+            )
             crossover_gene_index[random_gene_index_to_mutate] = True
 
-            trail_individual = np.where(crossover_gene_index, mutant, current_individual)
+            trail_individual = np.where(
+                crossover_gene_index, mutant, current_individual
+            )
             trail_individuals[individual_index] = trail_individual
             trial_fitnesses[individual_index] = fitness(trail_individual)
-            current_number_of_fitness_evaluations = current_number_of_fitness_evaluations + 1
+            current_number_of_fitness_evaluations = (
+                current_number_of_fitness_evaluations + 1
+            )
 
         for individual_index in range(population_size):
 
             if (
-                trial_fitnesses[individual_index] <=
-                previous_gen_individuals_fitnesses[individual_index]
+                trial_fitnesses[individual_index]
+                <= previous_gen_individuals_fitnesses[individual_index]
             ):
-                next_gen_individuals[individual_index] = trail_individuals[individual_index]
-                next_gen_individuals_fitnesses[individual_index] = trial_fitnesses[individual_index]
+                next_gen_individuals[individual_index] = trail_individuals[
+                    individual_index
+                ]
+                next_gen_individuals_fitnesses[individual_index] = trial_fitnesses[
+                    individual_index
+                ]
 
-                if trial_fitnesses[individual_index] < previous_gen_individuals_fitnesses[individual_index]:
+                if (
+                    trial_fitnesses[individual_index]
+                    < previous_gen_individuals_fitnesses[individual_index]
+                ):
                     if number_individuals_in_archive < archive_size:
-                        archive[number_individuals_in_archive] = trail_individuals[individual_index]
-                        archive_fitnesses[number_individuals_in_archive] = trial_fitnesses[individual_index]
+                        archive[number_individuals_in_archive] = trail_individuals[
+                            individual_index
+                        ]
+                        archive_fitnesses[number_individuals_in_archive] = (
+                            trial_fitnesses[individual_index]
+                        )
                     else:
                         random_element_index = np.random.randint(0, archive_size)
-                        archive[random_element_index] = trail_individuals[individual_index]
-                        archive_fitnesses[random_element_index] = trial_fitnesses[individual_index]
+                        archive[random_element_index] = trail_individuals[
+                            individual_index
+                        ]
+                        archive_fitnesses[random_element_index] = trial_fitnesses[
+                            individual_index
+                        ]
 
-                    succeding_mutation_rates[succeding_trials_count] = mutation_rates[individual_index]
-                    succeding_crossover_rates[succeding_trials_count] = crossing_rates[individual_index]
+                    succeding_mutation_rates[succeding_trials_count] = mutation_rates[
+                        individual_index
+                    ]
+                    succeding_crossover_rates[succeding_trials_count] = crossing_rates[
+                        individual_index
+                    ]
 
-                    succeding_trials[succeding_trials_count] = trail_individuals[individual_index]
-                    succeding_trial_fitnesses[succeding_trials_count] = trial_fitnesses[individual_index]
+                    succeding_trials[succeding_trials_count] = trail_individuals[
+                        individual_index
+                    ]
+                    succeding_trial_fitnesses[succeding_trials_count] = trial_fitnesses[
+                        individual_index
+                    ]
 
                     are_individuals_surpassed[individual_index] = 1
 
                     succeding_trials_count += 1
 
-                    if trial_fitnesses[individual_index] < global_best_individual_fitness:
+                    if (
+                        trial_fitnesses[individual_index]
+                        < global_best_individual_fitness
+                    ):
                         global_best_individual = trail_individuals[individual_index]
-                        global_best_individual_fitness = trial_fitnesses[individual_index]
+                        global_best_individual_fitness = trial_fitnesses[
+                            individual_index
+                        ]
 
         if succeding_trials_count > 0:
-            if (
-                np.nanmax(succeding_mutation_rates) <= 0 or
-                np.isnan(memory[memory_index_to_update, MEMORY_MCR_INDEX])
+            if np.nanmax(succeding_mutation_rates) <= 0 or np.isnan(
+                memory[memory_index_to_update, MEMORY_MCR_INDEX]
             ):
                 memory[memory_index_to_update, MEMORY_MCR_INDEX] = np.nan
             else:
                 memory[memory_index_to_update, MEMORY_MCR_INDEX] = weighted_lehmar_mean(
                     succeding_crossover_rates[:succeding_trials_count],
                     previous_gen_individuals_fitnesses[are_individuals_surpassed == 1],
-                    succeding_trial_fitnesses[:succeding_trials_count]
+                    succeding_trial_fitnesses[:succeding_trials_count],
                 )
-            
+
             memory[memory_index_to_update, MEMORY_MMR_INDEX] = weighted_lehmar_mean(
                 succeding_mutation_rates[:succeding_trials_count],
                 previous_gen_individuals_fitnesses[are_individuals_surpassed == 1],
-                succeding_trial_fitnesses[:succeding_trials_count]
+                succeding_trial_fitnesses[:succeding_trials_count],
             )
 
             memory_index_to_update = (memory_index_to_update + 1) % memory_size
 
         next_gen_population_size = round(
             (
-                (min_number_of_individuals - initial_population_size) /
-                max_number_fitness_evaluations
-            ) * current_number_of_fitness_evaluations + initial_population_size
+                (min_number_of_individuals - initial_population_size)
+                / max_number_fitness_evaluations
+            )
+            * current_number_of_fitness_evaluations
+            + initial_population_size
         )
 
         if next_gen_population_size < population_size:
             if next_gen_population_size < min_number_of_individuals:
-                next_gen_individuals = np.empty((0,2))
+                next_gen_individuals = np.empty((0, 2))
                 next_gen_individuals_fitnesses = np.empty(0)
             else:
-                number_of_individuals_to_delete = population_size - next_gen_population_size
+                number_of_individuals_to_delete = (
+                    population_size - next_gen_population_size
+                )
                 worst_individuals_indices = np.argpartition(
                     next_gen_individuals_fitnesses, -number_of_individuals_to_delete
                 )[:number_of_individuals_to_delete]
 
-                next_gen_individuals = np.delete(next_gen_individuals, worst_individuals_indices, axis=0)
+                next_gen_individuals = np.delete(
+                    next_gen_individuals, worst_individuals_indices, axis=0
+                )
                 next_gen_individuals_fitnesses = np.delete(
                     next_gen_individuals_fitnesses, worst_individuals_indices
                 )
                 population_size = next_gen_population_size
-        
+
         previous_gen_population = next_gen_individuals
         previous_gen_individuals_fitnesses = next_gen_individuals_fitnesses
         generation_number = generation_number + 1
